0f07f144909f3c0d1d32ecf865626c927cb77012
Saskia <gianosas@students.zhaw.ch>
2023-10-10 18:41:55 +0200

    kleinere Anpassungen

--- Auswertung.R
+++ Auswertung.R
@@ -1,15 +1,16 @@
-### Auswertungsskript Fallstudie Mikroalgen ##
+### Auswertungsskript Fallstudie Mikroalgen
+
 # benötigte Packages installieren
 install.packages("tidyverse")
 install.packages("tidyr")
 install.packages("ggplot2")
 install.packages("readr")
 
-#benötigte Packages laden
+# benötigte Packages laden
 library("tidyverse")
 library("tidyr")
 library("ggplot2")
 library("readr")
 
-#Daten einlesen
-read_csv("daten.csv", header = TRUE)
+# Daten einlesen
+df <- read_csv("daten.csv", header = TRUE)
