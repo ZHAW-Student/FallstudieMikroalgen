a7e877eda672c8a54d07909f4e35f580e65f042f
Saskia <gianosas@students.zhaw.ch>
2023-10-10 18:40:14 +0200

    Packages installieren und abrufen

--- Auswertung.R
+++ Auswertung.R
@@ -1,6 +1,15 @@
 ### Auswertungsskript Fallstudie Mikroalgen ##
-#benötigte Packages laden
+# benötigte Packages installieren
+install.packages("tidyverse")
+install.packages("tidyr")
+install.packages("ggplot2")
+install.packages("readr")
 
+#benötigte Packages laden
+library("tidyverse")
+library("tidyr")
+library("ggplot2")
+library("readr")
 
 #Daten einlesen
-
+read_csv("daten.csv", header = TRUE)
