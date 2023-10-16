f9c5daf17473320be5d879488066503613b71111
Saskia <gianosas@students.zhaw.ch>
2023-10-16 17:32:57 +0200

    lubridate

--- Auswertung.R
+++ Auswertung.R
@@ -5,12 +5,15 @@ install.packages("tidyverse")
 install.packages("tidyr")
 install.packages("ggplot2")
 install.packages("readr")
+install.packages("lubridate")
 
 # benötigte Packages laden
 library("tidyverse")
 library("tidyr")
 library("ggplot2")
 library("readr")
+library("lubridate")
+
 
 # Daten einlesen
 reactor <- read_csv("daten.csv") #Dateinamen anpassen, falls ; als Trennzeichen read_csv2 verwenden
@@ -19,3 +22,4 @@ labor <- read_csv("Labordaten.csv") #Dateinamen anpassen, falls ; als Trennzeich
 labor$Datum_Probenahme <- as.POSIXct(labor$Datum_Probenahme, format = "%d%m%Y")
 labor$Uhrzeit_Probenahme <- as.POSIXct(labor$Uhrzeit_Probenahme, format = "%H%M")
 labor$DateTime <- paste(labor$Datum_Probenahme, labor$Uhrzeit_Probenahme)
+duration()
