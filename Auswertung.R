3bf04bd4e9fb1856b5d7ece9ecf77ca7dfae07c4
Saskia <gianosas@students.zhaw.ch>
2023-10-16 17:26:12 +0200

    Vorbereitung Auswertung

--- Auswertung.R
+++ Auswertung.R
@@ -1,6 +1,6 @@
-### Auswertungsskript Fallstudie Mikroalgen
+### Auswertungsskript Fallstudie Mikroalgen ###
 
-# benötigte Packages installieren
+### benötigte Packages installieren ###
 install.packages("tidyverse")
 install.packages("tidyr")
 install.packages("ggplot2")
@@ -13,4 +13,9 @@ library("ggplot2")
 library("readr")
 
 # Daten einlesen
-df <- read_csv("daten.csv", header = TRUE)
+reactor <- read_csv("daten.csv") #Dateinamen anpassen, falls ; als Trennzeichen read_csv2 verwenden
+labor <- read_csv("Labordaten.csv") #Dateinamen anpassen, falls ; als Trennzeichen read_csv2 verwenden
+
+labor$Datum_Probenahme <- as.POSIXct(labor$Datum_Probenahme, format = "%d%m%Y")
+labor$Uhrzeit_Probenahme <- as.POSIXct(labor$Uhrzeit_Probenahme, format = "%H%M")
+labor$DateTime <- paste(labor$Datum_Probenahme, labor$Uhrzeit_Probenahme)
