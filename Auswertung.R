7136d84f9e50c799b7699ebf6cdf103b583d4ce2
Saskia <gianosas@students.zhaw.ch>
2023-10-23 08:03:18 +0200

    23.10

--- Auswertung.R
+++ Auswertung.R
@@ -4,6 +4,7 @@
 install.packages("tidyverse")
 install.packages("tidyr")
 install.packages("ggplot2")
+install.packages("ggpmisc")
 install.packages("readr")
 install.packages("lubridate")
 
@@ -11,18 +12,37 @@ install.packages("lubridate")
 library("tidyverse")
 library("tidyr")
 library("ggplot2")
+library("ggpmisc")
 library("readr")
 library("lubridate")
 
 ## Daten einlesen ####
 reactor <- read_delim("daten.csv", ";") #Dateinamen anpassen
-labor <- read_delim("Labordaten.csv", ";") #Dateinamen anpassen
+labordaten <- read_delim("Labordaten3.csv", ";") #Dateinamen anpassen
 
-# Daten formatieren und anpassen ####
-## Zeitformat anpassen und zusammenfügen ####
-labor$Datum_Probenahme <- as.POSIXct(labor$Datum_Probenahme, format = "%d%m%Y", tz = "UTC")
-labor$Uhrzeit_Probenahme <- as.POSIXct(labor$Uhrzeit_Probenahme, format = "%H%M", tz = "UTC")
-labor$DateTime <- paste(labor$Datum_Probenahme, labor$Uhrzeit_Probenahme)
+# neuen Dataframe erstellen mit den Daten aus dem csv, die uns weiter interessieren
+labor <- data.frame(Datum= as.character(labordaten$Datum_Probenahme))
+
+labor <- labor |> 
+  mutate(Zeit = as.character(labordaten$Zeitpunkt_Probenahme),
+         DatumZeit = as.character(paste(Datum, Zeit)),
+         DateTime = as.POSIXct(as.character(DatumZeit), format = "%d.%m.%Y %H:%M:%OS", tz = "UTC"),
+         Konzentration = as.numeric(labordaten$Konzentration_Algen_unbestimmt),
+         Trockenmasse = as.numeric(labordaten$Trockenmasse_gL),
+         log10Konz = log10(Konzentration))
+
+
+
+ggplot(labor, aes(x = Tag,y= Konzentration_Algen_unbestimmt)) + geom_line()
+ggplot(labor, aes(x=Tag, y=log10_Konzentration)) + geom_point() + geom_line() +
+  stat_smooth(method = "lm", formula = y~x, geom = "smooth")
+  stat_regline_equation
+
+## Wachstum nach Tag plotten mit Regressionsgeraden und Formel ####
+ggplot(labor, aes(x=Tag, y=log10Konz)) + geom_point() + geom_line() +  
+  stat_poly_line() +
+  stat_poly_eq(use_label(c("eq", "R2"))) +
+  geom_point()
 
 ## Faktoren erstellen ####
 
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # FallstudieMikroalgen
 Codes für die Fallstudie Mikroalgen ReMe HS23
 
-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
