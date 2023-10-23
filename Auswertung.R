c6e438ec9adfd4cd783ed19b4993f9cc2590d507
ZHAW-Student <gianosas@students.zhaw.ch>
2023-10-23 19:27:23 +0200

    Plot Trockenmasse

--- Auswertung.R
+++ Auswertung.R
@@ -18,7 +18,7 @@ library("lubridate")
 
 ## Daten einlesen ####
 reactor <- read_delim("daten.csv", ";") #Dateinamen anpassen
-labordaten <- read_delim("Labordaten3.csv", ";") #Dateinamen anpassen
+labordaten <- read_delim("Labordaten5.csv", ";") #Dateinamen anpassen
 
 # neuen Dataframe erstellen mit den Daten aus dem csv, die uns weiter interessieren
 labor <- data.frame(Datum= as.character(labordaten$Datum_Probenahme))
@@ -43,7 +43,30 @@ ggplot(labor, aes(x=Tage, y=log10Konz)) + geom_point() + geom_line() +
 ggplot(labor, aes(x=Tage, y=log10Konz)) + geom_point() + geom_line() +  
   stat_poly_line() +
   stat_poly_eq(use_label(c("eq", "R2"))) +
-  geom_point()
+  geom_point() +
+    labs(
+      x = "Anzahl Tage seit Experimentbeginn",
+      y = "log10 Algenkonzentration",
+      title = "Algenkonzentration über Zeit",
+      subtitle = "02. Oktober bis XX. November 2023"
+    )
 
-## Faktoren erstellen ####
+ggplot(labor, aes(x=Tage, y=Konzentration)) + geom_point() + geom_line() +  
+    geom_point() +
+    labs(
+      x = "Anzahl Tage seit Experimentbeginn",
+      y = "Algenkonzentration",
+      title = "Algenkonzentration über Zeit",
+      subtitle = "02. Oktober bis XX. November 2023"
+    )
 
+## Plot Trockenmasse
+ggplot(labor, aes(x=Tage, y=Trockenmasse)) + geom_point() + geom_line() +  
+  geom_point() +
+  labs(
+    x = "Anzahl Tage seit Experimentbeginn",
+    y = "Trockenmasse in g/l",
+    title = "Trockenmasse über Zeit",
+    subtitle = "02. Oktober bis XX. November 2023"
+  )
+  
